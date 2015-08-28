61c95b607f8d77cad2ff05962e00b108dc6d3f9f
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-08-28 16:44:10 +0200

    Private members.

--- js13kgames/src/element.js
+++ js13kgames/src/element.js
@@ -6,11 +6,22 @@
      * Describes abstract parent class for all construction elements.
      */
     Element = function(name) {
+        // Ensure being called with `new`
         if (!(this instanceof Element)) {
             return new Element();
         }
+
+        var feature;
+
         this.name = name;
         this.called = 0;
+
+        // feature is truly private here
+        // Doesn't work with Arrays and Objects (passed by reference!)
+        feature = 'sizzles';
+        this.getFeature = function() {
+            return feature;
+        };
     };
 
     Element.prototype.getName = function() {
