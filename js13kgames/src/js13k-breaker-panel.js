0253bedbfb70ea7b34e09492a8c1c13c75eeda7c
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-08-29 12:02:26 +0200

    Start programming.

---
+++ js13kgames/src/js13k-breaker-panel.js
@@ -0,0 +1,10 @@
+(function(global) {
+    "use strict";
+    var app, element, schalter;
+
+    // Shorthands
+    app = global.JS13KBP;
+    element = app.element;
+
+    schalter = new element.Element("schalter");
+})(this)
