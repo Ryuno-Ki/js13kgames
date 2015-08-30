036861567b7599928abe2f5a00680f5371074dbf
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-08-30 16:33:50 +0200

    Test circuit logic.

--- js13kgames/src/js/element.js
+++ js13kgames/src/js/element.js
@@ -7,11 +7,20 @@
         seProto,
         PowerSourceElement,
         pseProto,
-        circuitIsClosed,
-        renderCircuitLogic;
+        ConsumerElement,
+        ceProto,
+        CircuitElement,
+        cieProto;
 
     // FIXME: Use utils module!
-    var inherit, extendDeep, mix, inputSlotMixin, outputSlotMixin, ElectronicElementError;
+    var inherit,
+        extendDeep,
+        mix,
+        inputSlotMixin,
+        outputSlotMixin,
+        PowerSourceElementError,
+        ConsumerElementError,
+        ElectronicElementError;
 
     // FIXME: Use utils.inherit, utils.extendDeep, utils.mix and errors.ElectronicElementError!
     inherit = (function() {
@@ -70,6 +79,22 @@
         }
     };
 
+    PowerSourceElementError = {
+        name: "PowerSourceElementError",
+        message: "Must be an instance of PowerSourceElement!",
+        toString: function() {
+            return this.name + ": " + this.message;
+        }
+    };
+
+    ConsumerElementError = {
+        name: "ConsumerElementError",
+        message: "Must be an instance of ConsumerElement!",
+        toString: function() {
+            return this.name + ": " + this.message;
+        }
+    };
+
     inputSlotMixin = {
         setInput: function(element) {
             if (!(element instanceof ElectronicElement)) {
@@ -77,12 +102,14 @@
                 throw ElectronicElementError;
             }
             this._input = element;
+            // Using element.setOutput() yields an infinite loop
+            element._output = this;
         },
         getInput: function() {
             return this._input;
         },
         hasInput: function() {
-            return this._input !== null;
+            return (typeof this._input !== "undefined") && (this._input !== null);
         }
     };
 
@@ -93,12 +120,14 @@
                 throw ElectronicElementError;
             }
             this._output = element;
+            // Using element.setInput() yields an infinite loop
+            element._input = this;
         },
         getOutput: function() {
             return this._output;
         },
         hasOutput: function() {
-            return this._output !== null;
+            return (typeof this._output !== "undefined") && (this._output !== null);
         }
 
     };
@@ -118,7 +147,7 @@
         that = this;
         ElectronicElement.count += 1;
         that._name = name + '-' + ElectronicElement.count;
-        that._type = 'element';
+        that._type = 'electronic';
 
         // feature is truly private here
         // Doesn't work with Arrays and Objects (passed by reference!)
@@ -141,13 +170,7 @@
         return this._type;
     };
 
-    eeProto.hasInput = function() {
-        return false;
-    };
-
-    eeProto.hasOutput = function() {
-        return false;
-    };
+    extendDeep(mix(inputSlotMixin, outputSlotMixin), eeProto);
 
     eeProto.renderSelf = function(node) {
         var img, that;
@@ -212,6 +235,7 @@
         }
 
         that = this;
+        that._type = 'power-source';
         that._output = null;
     };
     inherit(PowerSourceElement, ElectronicElement);
@@ -219,21 +243,154 @@
     pseProto = PowerSourceElement.prototype;
     extendDeep(outputSlotMixin, pseProto);
 
-    circuitIsClosed = function(elements) {
-        var closedElements;
+    ConsumerElement = function(name) {
+        var that;
 
-        // Map ElectronicElement instances to Array of booleans according to their isClosed
-        closedElements = elements.map(function(el) {
-            var isClosed, hasInput, hasOutput;
+        // Ensure being called with `new`
+        if (!(this instanceof ConsumerElement)) {
+            return new ConsumerElement(name);
+        }
+
+        that = this;
+        that._type = 'consumer';
+        that._input = null;
+    };
+    inherit(ConsumerElement, ElectronicElement);
+
+    ceProto = ConsumerElement.prototype;
+    extendDeep(inputSlotMixin, ceProto);
+
+    CircuitElement = function(name) {
+        var that;
+
+        // Ensure being called with `new`
+        if (!(this instanceof CircuitElement)) {
+            return new CircuitElement(name);
+        }
+
+        that = this;
+        that._input = null;
+        that._type = 'circuit';
+        that._elements = [];
+    };
+    inherit(CircuitElement, ElectronicElement);
+
+    cieProto = CircuitElement.prototype;
+    cieProto.hasPowerSource = function() {
+        var elementNames;
+
+        elementNames = this._elements.map(function(el) {
             if (!(el instanceof ElectronicElement)) {
                 // TODO: Move into errors.js
                 throw ElectronicElementError;
             }
-            isClosed = el.isClosed();
-            hasInput = el.getInput() !== null;
-            hasOutput = el.getOutput() !== null;
-            return isClosed && hasInput && hasOutput;
+            return el.getType();
         });
+        return elementNames.indexOf("power-source") !== -1;
+    };
+
+    cieProto.addPowerSource = function(powerSource) {
+        if (!(powerSource instanceof PowerSourceElement)) {
+            // TODO: Move into errors.js
+            throw PowerSourceElementError;
+        }
+        this._elements.push(powerSource);
+    };
+
+    cieProto.hasConsumer = function() {
+        var elementNames;
+
+        elementNames = this._elements.map(function(el) {
+            if (!(el instanceof ElectronicElement)) {
+                // TODO: Move into errors.js
+                throw ElectronicElementError;
+            }
+            return el.getType();
+        });
+        return elementNames.indexOf("consumer") !== -1;
+    };
+
+    cieProto.addConsumer = function(consumer) {
+        if (!(consumer instanceof ConsumerElement)) {
+            // TODO: Move into errors.js
+            throw ConsumerElementError;
+        }
+        this._elements.push(consumer);
+    };
+
+    cieProto.hasElectronic = function() {
+        var elementNames, isElectronicElement, i, len;
+
+        elementNames = this._elements.map(function(el) {
+            if (!(el instanceof ElectronicElement)) {
+                // TODO: Move into errors.js
+                throw ElectronicElementError;
+            }
+            return el.getType();
+        });
+
+        isElectronicElement = function(elementName) {
+            var electronics;
+
+            // ToDo: Move this Array out of here!
+            electronics = ["electronic", "switch"];
+            if (electronics.indexOf(elementName) !== -1) {
+                return true;
+            }
+            return false;
+        };
+
+        for (i = 0, len = elementNames.length; i < len; i += 1) {
+            if(isElectronicElement(elementNames[i])) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    cieProto.addElectronic = function(electronic) {
+        if (!(electronic instanceof ElectronicElement)) {
+            // TODO: Move into errors.js
+            throw ElectronicElementError;
+        }
+        this._elements.push(electronic);
+    };
+
+    cieProto._mapElementsToLogic = function() {
+        // Map ElectronicElement instances to Array of booleans according to their isClosed
+        return  this._elements.map(function(el) {
+            if (!(el instanceof ElectronicElement)) {
+                // TODO: Move into errors.js
+                throw ElectronicElementError;
+            }
+
+            switch (el.getType()) {
+                case "power-source":
+                    return el.hasOutput() && (el.getOutput() !== null);
+                case "consumer":
+                    return el.hasInput() && (el.getInput() !== null);
+                case "electronic":
+                    return el.hasInput() && (el.getInput() !== null) && el.hasOutput() && (el.getOutput() !== null);
+                case "switch":
+                    return el.hasInput() && (el.getInput() !== null) && el.hasOutput() && (el.getOutput() !== null) && el.isClosed();
+                default:
+                    return false;
+            }
+        });
+    };
+
+    cieProto.isCompleteCircuit = function() {
+        return this.hasPowerSource() && this.hasConsumer() && this.hasElectronic();
+    };
+
+    cieProto.isCircuitClosed = function() {
+        var closedElements;
+
+        if (!this.isCompleteCircuit()) {
+            return false;
+        }
+
+        closedElements = this._mapElementsToLogic();
 
         // Concat every isClosed() return value
         return closedElements.reduce(function(current, former) {
@@ -241,7 +398,7 @@
         });
     };
 
-    renderCircuitLogic = function(elements) {
+    cieProto.renderCircuitLogic = function() {
         var closedElements, logic, logicMap, i, len, current, next;
 
         logicMap = {
@@ -250,27 +407,20 @@
             not: '\u00AC'
         };
 
-        // Map ElectronicElement instances to Array of booleans according to their isClosed
-        closedElements = elements.map(function(el) {
-            var isClosed, hasInput, hasOutput;
-            if (!(el instanceof ElectronicElement)) {
-                // TODO: Move into errors.js
-                throw ElectronicElementError;
-            }
-            isClosed = el.isClosed();
-            hasInput = el.getInput() !== null;
-            hasOutput = el.getOutput() !== null;
-            return isClosed && hasInput && hasOutput;
-        });
+        closedElements = this._mapElementsToLogic();
 
         // Concat every isClosed() return value
-        logic = +closedElements[0];
+        if (closedElements.length) {
+            logic = +closedElements[0];
+        } else {
+            logic = 0;
+        }
         for (i = 0, len = closedElements.length - 1; i < len; i += 1) {
             current = closedElements[i];
             next = closedElements[i+1];
             logic += ' ' + logicMap.and + ' ' + Number(next);
         }
-        logic += ' = ' + Number(circuitIsClosed(elements));
+        logic += ' = ' + Number(this.isCircuitClosed());
         return logic;
     };
 
@@ -282,8 +432,8 @@
         ElectronicElement: ElectronicElement,
         SwitchElement: SwitchElement,
         PowerSourceElement: PowerSourceElement,
-        circuitIsClosed: circuitIsClosed,
-        renderCircuitLogic: renderCircuitLogic
+        ConsumerElement: ConsumerElement,
+        CircuitElement: CircuitElement,
     };
     return ns;
 })(this);
--- js13kgames/test/element/element.spec.js
+++ js13kgames/test/element/element.spec.js
@@ -37,15 +37,15 @@ describe('ElectronicElement', function() {
     });
 
     it('should save its type', function() {
-        expect(el.getType()).to.equal('element');
+        expect(el.getType()).to.equal('electronic');
     });
 
-    it('should tell about whether it has an input slot', function() {
+    it('should have no input slot', function() {
         expect(el.hasInput).to.be.a('function');
         expect(el.hasInput()).to.be.false;
     });
 
-    it('should tell about whether it has an output slot', function() {
+    it('should have no output slot', function() {
         expect(el.hasOutput).to.be.a('function');
         expect(el.hasOutput()).to.be.false;
     });
@@ -142,7 +142,8 @@ describe("Power source", function() {
     });
 
     it('should have no input slot', function() {
-        expect(el.getInput).to.be.undefined;
+        expect(el.hasInput).to.be.a('function');
+        expect(el.hasInput()).to.be.false;
     });
 
     it('should have an output slot', function() {
@@ -151,65 +152,142 @@ describe("Power source", function() {
     });
 });
 
+describe("ConsumerElement", function() {
+    var el, name;
+
+    name = "consumer";
+
+    it("should have a constructor", function() {
+        expect(element.ConsumerElement).to.be.a("function");
+    });
+
+    beforeEach(function() {
+        el = new element.ConsumerElement(name);
+    });
+
+    it("should inherit from ElectronicElement", function() {
+        expect(el instanceof element.ConsumerElement).to.be.true;
+        expect(el instanceof element.ElectronicElement).to.be.true;
+    });
+
+    it("should have an input slot", function() {
+        expect(el.getInput()).not.to.be.undefined;
+        expect(el.getInput()).to.be.null;
+    });
+
+    it("should have no output slot", function() {
+        expect(el.hasOutput).to.be.a("function");
+        expect(el.hasOutput()).to.be.false;
+    });
+});
+
 describe("Circuit", function() {
-    describe("containing out of switches", function() {
-        var el1, el2, el3;
-    
+    var el, name;
+
+    name = "circuit";
+
+    it("should have a constructor", function() {
+        expect(element.CircuitElement).to.be.a("function");
+    });
+
+    beforeEach(function() {
+        el = new element.CircuitElement(name);
+    });
+
+    it("should inherit from ElectronicElement", function() {
+        expect(el instanceof element.CircuitElement).to.be.true;
+        expect(el instanceof element.ElectronicElement).to.be.true;
+    });
+
+    it("should contain at least one power source", function() {
+        var pse;
+
+        expect(el.hasPowerSource()).to.be.false;
+        pse = new element.PowerSourceElement();
+        el.addPowerSource(pse);
+        expect(el.hasPowerSource()).to.be.true;
+    });
+
+    it("should contain at lease one consumer", function() {
+        var ce;
+
+        expect(el.hasConsumer()).to.be.false;
+        ce = new element.ConsumerElement();
+        el.addConsumer(ce);
+        expect(el.hasConsumer()).to.be.true;
+    });
+
+    describe("with a power source, a consumer and at least one electronic element", function() {
+        var pse, ce, ee;
+
         beforeEach(function() {
-            el1 = new element.SwitchElement("element1");
-            el2 = new element.SwitchElement("element2");
-            el3 = new element.SwitchElement("element3");
+            pse = new element.PowerSourceElement();
+            ce = new element.ConsumerElement();
+            ee = new element.ElectronicElement();
         });
-    
-        it('should contain out of closed switches', function() {
-            expect(el1.isClosed()).to.be.true;
-            expect(el2.isClosed()).to.be.true;
-            expect(el3.isClosed()).to.be.true;
+
+        it("should demand them", function() {
+            expect(el.isCompleteCircuit()).to.be.false;
+
+            el.addPowerSource(pse);
+            expect(el.isCompleteCircuit()).to.be.false;
+
+            el.addConsumer(ce);
+            expect(el.isCompleteCircuit()).to.be.false;
+
+            el.addElectronic(ee);
+            expect(el.isCompleteCircuit()).to.be.true;
         });
+
+        context("when plugged together", function() {
+            it("should ensure that", function() {
+                var se;
     
-        describe("when not plugged together", function() {
-            it('should form an open circuit', function() {
-                expect(el1.hasInput()).to.be.false;
-                expect(el2.hasInput()).to.be.false;
-                expect(el3.hasInput()).to.be.false;
+                se = new element.SwitchElement();
+                expect(se.isClosed()).to.be.true;
     
-                expect(el1.hasOutput()).to.be.false;
-                expect(el2.hasOutput()).to.be.false;
-                expect(el3.hasOutput()).to.be.false;
+                expect(se.getInput()).to.be.null;
+                se.setInput(pse);
+                expect(se.getInput().getName()).to.equal(pse.getName());
+                expect(pse.getOutput().getName()).to.equal(se.getName());
     
-                expect(element.circuitIsClosed([el1, el2, el3])).to.be.false;
+                expect(se.getOutput()).to.be.null;
+                se.setOutput(ce);
+                expect(se.getOutput().getName()).to.equal(ce.getName());
+                expect(ce.getInput().getName()).to.equal(se.getName());
             });
     
-            it('should render a formula showing it', function() {
-                expect(element.renderCircuitLogic([el1, el2, el3])).to.equal('0 \u2227 0 \u2227 0 = 0');
-            });
-        });
-    
-        describe("when plugged together", function() {
-            beforeEach(function() {
-                el1.setInput(el3);
-                el2.setInput(el1);
-                el3.setInput(el2);
-    
-                el1.setOutput(el2);
-                el2.setOutput(el3);
-                el3.setOutput(el1);
-            });
-    
-            it('should form a closed circuit', function() {
-                expect(el1.hasInput()).to.be.true;
-                expect(el2.hasInput()).to.be.true;
-                expect(el3.hasInput()).to.be.true;
-    
-                expect(el1.hasOutput()).to.be.true;
-                expect(el2.hasOutput()).to.be.true;
-                expect(el3.hasOutput()).to.be.true;
-    
-                expect(element.circuitIsClosed([el1, el2, el3])).to.be.true;
-            });
-    
-            it('should render a formula showing it', function() {
-                expect(element.renderCircuitLogic([el1, el2, el3])).to.equal('1 \u2227 1 \u2227 1 = 1');
+            context("and inserted in the circuit", function() {
+                var se;
+
+                beforeEach(function() {
+                    se = new element.SwitchElement();
+                    el.addPowerSource(pse);
+                    el.addConsumer(ce);
+                    el.addElectronic(se);
+                });
+
+                it("should ensure, that the circuit is closed", function() {
+                    expect(se.isClosed()).to.be.true;
+
+                    se.setInput(pse);
+                    se.setOutput(ce);
+                    expect(el.isCircuitClosed()).to.be.true;
+
+                    se.useSwitch();
+                    expect(se.isClosed()).to.be.false;
+                    expect(el.isCircuitClosed()).to.be.false;
+                });
+
+                it('should render a formula showing it', function() {
+                    // Order in which they were inserted, see beforeEach hook
+                    expect(el.renderCircuitLogic()).to.equal('0 \u2227 0 \u2227 0 = 0');
+                    se.setInput(pse);
+                    se.setOutput(ce);
+                    expect(el.renderCircuitLogic()).to.equal('1 \u2227 1 \u2227 1 = 1');
+                    se.useSwitch();
+                    expect(el.renderCircuitLogic()).to.equal('1 \u2227 1 \u2227 0 = 0');
+                });
             });
         });
     });
