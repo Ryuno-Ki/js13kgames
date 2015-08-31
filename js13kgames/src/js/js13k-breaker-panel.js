3f6d3c044a2d120ef7eab2d58d144d290f2dffef
André Jaenisch <andre.jaenisch@openmailbox.org>
2015-08-31 22:33:49 +0200

    Script SVG

--- js13kgames/src/js/js13k-breaker-panel.js
+++ js13kgames/src/js/js13k-breaker-panel.js
@@ -1,6 +1,6 @@
 (function(global) {
     "use strict";
-    var game, app, element, switchElement, se, e, logic;
+    var game, app, element, ps, switchElement, se, e, circuit, c, logic, svg, rect, circle;
 
     game = document.getElementById("game");
 
@@ -8,14 +8,46 @@
     app = global.JS13KBP;
     element = app.element;
 
+    ps = new element.PowerSourceElement("power-source");
     switchElement = new element.SwitchElement("switchElement");
     se = new element.SwitchElement("se");
     e = new element.SwitchElement("e");
+    circuit = new element.CircuitElement("circuit");
+    c = new element.ConsumerElement("consumer");
 
     switchElement.setInput(se);
     se.setInput(e);
 
-    logic = document.createElement('span');
-    logic.innerHTML += element.renderCircuitLogic([switchElement, se, e]);
-    game.appendChild(logic);
+    circuit.addPowerSource(ps);
+    circuit.addElectronic(switchElement);
+    circuit.addElectronic(se);
+    circuit.addElectronic(e);
+    circuit.addConsumer(c);
+
+    logic = document.createElement('div');
+    logic.innerHTML += circuit.renderCircuitLogic();
+    // game.appendChild(logic);
+
+    svg = document.createElement('svg');
+    svg.setAttribute("viewBox", "0 0 100 100");
+    svg.setAttribute("version", "1.1");
+    svg.setAttribute("xmlns", "http://www,w3.org/2000/svg");
+    svg.setAttribute("width", "100");
+    svg.setAttribute("height", "100");
+    game.appendChild(svg);
+
+    rect = document.createElement('rect');
+    rect.setAttribute("x", "10");
+    rect.setAttribute("y", "20");
+    rect.setAttribute("width", "30");
+    rect.setAttribute("height", "40");
+
+    circle = document.createElement("circle");
+    circle.setAttribute("cx", "60");  // Coordinate x
+    circle.setAttribute("cy", "60");  // Coordinate y
+    circle.setAttribute("r", "40");   // Radius
+
+    svg.appendChild(rect);
+    svg.appendChild(circle);
+    svg.outerHTML = svg.outerHTML;  // Enforce repaint
 })(this);
